aaf98c3fd4bdc94319a8239ecb4e0b8a4375cfdd
maojunqi <504253012@qq.com>
2023-02-14 08:11:29 +0800

    isnever

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,6 @@
-type AnyOf<T extends readonly any[]> = any
+type FalseValues = "" | 0 | false | [] | null | undefined | Record<any, never>
+type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+  ? F extends FalseValues
+    ? AnyOf<R>
+    : true
+  : false
